42908444c0098e6273e749f3c45d578fe1bec32a
Ethanicuss <ethancranmer@utexas.edu>
2017-04-05 20:29:29 -0500

    made group class

---
+++ Guestbook/src/main/java/studyR/Group.java
@@ -0,0 +1,48 @@
+package studyR;
+
+import java.util.ArrayList;
+import com.google.appengine.api.users.User;
+import com.googlecode.objectify.ObjectifyService;
+import com.googlecode.objectify.annotation.Id;
+
+public class Group{
+	@Id Long id;
+	private String name;
+	private ArrayList<Profile> members = new ArrayList<Profile>();
+	private String groupBio = "No bio available.";
+	
+	public Group(String name){
+		this.name = name;
+	}
+	
+	public Group(String name, String groupBio){
+		this(name);
+		this.groupBio = groupBio;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public ArrayList<Profile> getMembers() {
+		return members;
+	}
+
+	public void setMembers(ArrayList<Profile> members) {
+		this.members = members;
+	}
+
+	public String getGroupBio() {
+		return groupBio;
+	}
+
+	public void setGroupBio(String groupBio) {
+		this.groupBio = groupBio;
+	}
+	
+	
+}
