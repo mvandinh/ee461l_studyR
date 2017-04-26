c221b1c0b4d523e2db40cc50e2ee7f61cb159be1
Ethanicuss <ethancranmer@utexas.edu>
2017-04-25 21:31:54 -0500

    objectified Course

--- Guestbook/src/main/java/studyR/Course.java
+++ Guestbook/src/main/java/studyR/Course.java
@@ -1,16 +1,15 @@
 package studyR;
 
+import static com.googlecode.objectify.ObjectifyService.ofy;
 import com.googlecode.objectify.ObjectifyService;
 import com.googlecode.objectify.annotation.Id;
 
 public class Course {
 
-    @Id Long id;
-	String courseName;
+    @Id String courseName;
 	int uniqueID;
 	
 	public Course(){
-		id = (long) 0; //TODO change this obviously
 		courseName = "UNTITLED";
 		uniqueID = 0;
 	}
@@ -18,6 +17,7 @@ public class Course {
 	public Course(String courseName, int uniqueID) {
 		this.courseName = courseName;
 		this.uniqueID = uniqueID;
+		ofy().save().entity(this).now();
 	}
 	
 	public String getCourseName() {
